30fb76dfe3a648d7103231cf1e68ecb15a9b6241
Shaun Ryan <shaunryan0701@googlemail.com>
2025-07-15 13:27:19 +0100

    Added sorting examples

---
+++ sorting/assignment.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+prices = np.array([5.99, 6.99, 22.49, 99.99, 4.99, 49.99])
+
+print(prices)
+
+prices.sort()
+
+high_prices = prices[:2:-1]
+print("High prices:", high_prices)
+
+# Output the mean of the prices
+mean_price = np.mean(high_prices)
+min_price = np.min(high_prices)
+max_price = np.max(high_prices)
+print("Mean price:", mean_price)
+print("Min price:", min_price)
+print("Max price:", max_price)
