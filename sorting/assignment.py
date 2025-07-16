07ead9178213dd01baedf040a36b8175d63522ec
Shaun Ryan <shaunryan0701@googlemail.com>
2025-07-16 22:09:06 +0100

    Updated sorting examples

--- sorting/assignment.py
+++ sorting/assignment.py
@@ -6,13 +6,15 @@ print(prices)
 
 prices.sort()
 
-high_prices = prices[:2:-1]
+high_prices = prices[-3:]
 print("High prices:", high_prices)
 
 # Output the mean of the prices
 mean_price = np.mean(high_prices)
 min_price = np.min(high_prices)
 max_price = np.max(high_prices)
+median_price = np.median(high_prices)
 print("Mean price:", mean_price)
 print("Min price:", min_price)
 print("Max price:", max_price)
+print("Median price:", median_price)
