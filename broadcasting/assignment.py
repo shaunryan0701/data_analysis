3b30e580a6c8f0849774604c6900ef482a9bb48f
Shaun Ryan <shaunryan0701@googlemail.com>
2025-07-25 14:05:04 +0100

    Added broadcasting examples

---
+++ broadcasting/assignment.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# Create an array of sales
+sales = np.array([100, 200, 300, 400, 500])
+
+family = np.array([])
---
+++ broadcasting/exercise.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+# Create an array of ages
+ages = np.array([5, 10, 25, 34, 49, 60])
+
+age_increment = 5
+# Increment all ages by 5   
+
+updated_ages = ages + age_increment
+
+print("Updated ages:", updated_ages)
